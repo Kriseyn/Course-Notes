d9d974febe69ae3add26e49543168815a2319fab
Kriseyn <annekriscapito@gmail.com>
2014-09-20 23:12:06 -0700

    mynotes

---
+++ src/edu/ucuccs/mycoursenote/Notes_Alarm.java
@@ -0,0 +1,15 @@
+package edu.ucuccs.mycoursenote;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Notes_Alarm extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_notes__alarm);
+	}
+
+}
